151c10d33d45efecdd505b176bb2ed5dd1638e48
BettyPan <betty19931218@gmail.com>
2020-11-04 14:17:56 +0800

    Initial Commit

---
+++ AnimalCrossing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalCrossing
+//
+//  Created by Stanley on 2020/11/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
